ad348185d4f52b2f7b0a4bf181247eb91595196a
Wright <calebogundiya@gmail.com>
2020-07-18 13:20:34 +0100

    Implicit assignment of dynamic arrays not possible

--- arraysContract.sol
+++ arraysContract.sol
@@ -12,7 +12,7 @@ contract ArraysContract {
     // Dynamic array of type bool
     bool[] dynamicBoolArray;
     
-    function testArray() public {
+    function testArray() public returns (uint256){
         // Allocate memory for 8 elements to the dynamic bool storage array
         dynamicBoolArray = new bool[](8);
         
@@ -20,6 +20,21 @@ contract ArraysContract {
         // explicit conversion is needed from uint8 to int8
         dynamicInt8Array = [int8(1), 2, 3];
         
+        // works fine since we're inside a function
+        uint8[] memory memoryArray; // A dynamic array in memory
         
+        // Allocation with assignment not allowed for dynamic memory Arrays
+        // memoryArray.push = 3;
+        
+        uint8[] memory dynamicMemoryArray; // A dynamic memory array
+        // Assignment of dynamic NOT allowed
+        // dynamicMemoryArray = [uint8(1), 2];
+        
+        memoryArray = new uint8[](8);
+        // memoryArray.push(10);
+        // return dynamicInt8Array.length;
+        // memoryArray.length = 6;
+        
+        /** Examples of array inititalization with assignment below */
     }
 }
