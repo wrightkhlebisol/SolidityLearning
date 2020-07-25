eac1c263264483670474e9182d40606ea2f86a54
Wright <calebogundiya@gmail.com>
2020-07-25 22:55:12 +0100

    Struct Types - Strictly typed enums

---
+++ structTypes.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.4.4;
+
+contract StructsContract {
+    
+    // Country
+    struct family {
+        bytes32 lastName;
+        uint8 houseNo;
+        uint16 age;
+        
+    }
+    
+    // creating an array of type family..which is a struct
+    family [] myFamily;
+    
+    // Gettng a lastName, and returning complete
+    // family details
+    // We cannot compare 2 strings in solidity
+    function getName(bytes32 name) public view returns (bytes32, uint8, uint16){
+        // Search the array
+        
+        for(uint8 i = 0; i < myFamily.length; i++){
+            if(name == myFamily[i].lastName){
+                return (myFamily[i].lastName, uint8(myFamily[i].houseNo), myFamily[i].age);
+            }
+        }
+    }
+    
+    function addName(bytes32 _lastName, uint8 _value, uint16 _age) public returns (bool){
+        // Declare the struct variable in memory
+        family memory newFamily;
+        
+        // use the . notation to access members of a struct
+        newFamily.lastName = _lastName;
+        newFamily.houseNo = _value;
+        newFamily.age = _age;
+        
+        // Push the newFamily struct...into our myFamily array
+        myFamily.push(newFamily);
+        return true;
+    }
+}
