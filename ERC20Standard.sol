296ee8daa5d21b830bfca1e61ac5ca4c942bc4cc
Wright <calebogundiya@gmail.com>
2020-08-01 15:22:11 +0100

    ERC-20 NFT standards interface

---
+++ ERC20Standard.sol
@@ -0,0 +1,19 @@
+pragma solidity >=0.4.24;
+
+contract ERC20Standard {
+
+    string public constant name;
+    string public constant symbol;
+    uint8 public constant decimals;  // 18 is the most common number of decimal places
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+
+    function totalSupply() public constant returns (uint);
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+
+}
